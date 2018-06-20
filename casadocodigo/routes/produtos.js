b5306a42a624d31c46f872a6b4d3a4107527870c
G. Oliveira Delmiglio <g.oliveira.delmiglio@avanade.com>
2018-06-20 20:23:13 -0300

    Ajuste de Conexao Mysql

--- casadocodigo/routes/produtos.js
+++ casadocodigo/routes/produtos.js
@@ -1,5 +1,21 @@
-module.exports = function(app){
-    app.get('/produtos', function(req,res){
-        res.render('produtos/lista');
+module.exports = function (app) {
+    app.get('/produtos', function (req, res) {
+
+        var mysql = require('mysql');
+
+        var connection = mysql.createConnection({
+            host: 'localhost',
+            user: 'root',
+            password: '123456',
+            database: 'casadocodigo'
+        });
+
+        connection.query('Select * From livros', (err,result,fields ) =>{
+            res.send(result);
+        });
+
+        connection.end();
+
+        // res.render('produtos/lista');
     })
 }
