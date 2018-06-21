d5898f99ddd09b773d1e0c613291557fcb737b05
G. Oliveira Delmiglio <g.oliveira.delmiglio@avanade.com>
2018-06-20 21:55:20 -0300

    Connection Factory

---
+++ casadocodigo/infra/connectionFactory.js
@@ -0,0 +1,13 @@
+var mysql = require('mysql');
+
+function connectionFactory() {
+    const connection = mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: '123456',
+        database: 'casadocodigo'
+    });
+    return connection;
+}
+
+module.exports = connectionFactory;
--- casadocodigo/routes/produtos.js
+++ casadocodigo/routes/produtos.js
@@ -1,22 +1,14 @@
+const connectionFactory = require('../infra/connectionFactory')
+
 module.exports = function (app) {
     app.get('/produtos', function (req, res) {
 
-        var mysql = require('mysql');
-
-        var connection = mysql.createConnection({
-            host: 'localhost',
-            user: 'root',
-            password: '123456',
-            database: 'casadocodigo'
-        });
+        const connection = connectionFactory();
 
         connection.query('Select * From livros', (err, result, fields ) =>{
-            console.log(err);
             res.render('produtos/lista', {livros: result});
         });
 
         connection.end();
-
-        // res.render('produtos/lista');
     })
 }
